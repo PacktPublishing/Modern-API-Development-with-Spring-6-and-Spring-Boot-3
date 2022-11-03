a9f2654eb25e174178df1fc55d0178802f6b535c
sharmasourabh <sharmasourabh@users.noreply.github.com>
2022-11-03 22:25:47 +0530

    Chapter 03

---
+++ Chapter03/src/main/java/com/packt/modern/api/AppConfig.java
@@ -0,0 +1,20 @@
+package com.packt.modern.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : github.com/sharmasourabh
+ * @project : Chapter03 - Modern API Development with Spring and Spring Boot
+ * @created : 31/10/2022, Monday
+ **/
+public class AppConfig implements WebMvcConfigurer {
+
+/*  @Override
+  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    configurer.defaultContentType(MediaType.APPLICATION_JSON).
+        mediaType("xml", MediaType.APPLICATION_XML).
+        mediaType("json", MediaType.APPLICATION_JSON)
+  }*/
+}
